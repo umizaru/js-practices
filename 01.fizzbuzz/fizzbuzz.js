9f3ff61bf54ac786700bff9a7723ec92762d4da3
umizaru <y.wakita1T@gmail.com>
2023-02-27 15:02:47 +0900

    配列numbersを削除

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,15 +1,11 @@
-let numbers = [];
 for (let i = 1; i <= 20; i++) {
-  numbers.push(i);
-}
-numbers.forEach((number) => {
-  if (number % 15 == 0) {
+  if (i % 15 == 0) {
     console.log("FizzBuzz");
-  } else if (number % 5 == 0) {
+  } else if (i % 5 == 0) {
     console.log("Buzz");
-  } else if (number % 3 == 0) {
+  } else if (i % 3 == 0) {
     console.log("Fizz");
   } else {
-    console.log(number);
+    console.log(i);
   }
-});
+}
