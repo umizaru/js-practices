66af00bfd9c10c88070e70aad060e98575564e83
umizaru <y.wakita1T@gmail.com>
2023-02-28 16:39:04 +0900

    letをconstに修正、temlate literalに修正、表示部分の処理を修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 const { DateTime } = require("luxon");
-let args = require("minimist")(process.argv.slice(2));
+const args = require("minimist")(process.argv.slice(2));
 let year = args["y"];
 let month = args["m"];
 
@@ -15,24 +15,19 @@ if (month == undefined) {
 console.log("     ", month.toString() + "月", year.toString());
 console.log("日", "月", "火", "水", "木", "金", "土");
 
-let first_day = DateTime.fromObject({ year, month, day: 1 });
-let last_day = first_day.endOf("month");
-
-let days_of_month = [];
-for (let i = 1; i <= last_day.day; i++) {
-  days_of_month.push(i);
-}
+const first_day = DateTime.fromObject({ year, month, day: 1 });
+const last_day = first_day.endOf("month");
 
 let weekday_count = first_day.weekday;
-let spaces = "   ".repeat(weekday_count);
+const spaces = "   ".repeat(weekday_count);
 if (weekday_count !== 7) process.stdout.write(spaces);
 
-days_of_month.forEach((day) => {
-  let str_day = String(day).padStart(2, " ");
-  process.stdout.write(str_day + " ");
+for (let i = 1; i <= last_day.day; i++) {
+  const str_day = String(i).padStart(2, " ");
+  process.stdout.write(`${str_day} `);
   weekday_count++;
   if (weekday_count % 7 == 0) {
     console.log("");
   }
-});
+}
 console.log("\n");
