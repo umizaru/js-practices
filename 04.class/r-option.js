9a1a5ae90932933872ed8e4be182d0dd8abb3645
umizaru <y.wakita1T@gmail.com>
2023-04-30 01:14:54 +0900

    一覧表示を実装

--- 04.class/r-option.js
+++ 04.class/r-option.js
@@ -8,11 +8,9 @@ const displayMemos = () => {
   const jsonPath = "./data/memos.json";
   const jsonData = fs.readFileSync(jsonPath, "utf-8");
   const memosData = JSON.parse(jsonData);
-  const memos = memosData.memos;
 
-  for (let i in memos) {
-    const memosArray = memos[i].memo.split("\n");
-    console.log(memosArray[0]);
-  }
+  memosData.memos.forEach((memo) => {
+    console.log(memo.memo);
+  });
 };
 displayMemos();
